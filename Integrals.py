6f77e84479b075841f17c5cf1236a37142619eff
Михаил <mixxa0911@gmail.com>
2023-05-26 20:23:26 +0300

    Integrals

---
+++ Integrals.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+def __square_left_integral(f, a, b):
+    return f(a) * (b - a)
+
+
+def __square_mid_integral(f, a, b):
+    return f((b + a) / 2) * (b - a)
+
+
+def __square_right_integral(f, a, b):
+    return f(b) * (b - a)
+
+
+def __simp_integral(f, a, b):
+    return (b - a) * (f(a) + 4 * f((a + b) / 2) + f(b)) / 6
+
+
+def rec_square_integral(f, a, b, n):
+    delta = (b - a) / n
+    l_sum = sum([__square_left_integral(f, i, i + delta) for i in np.arange(a, b, delta)])
+    m_sum = sum([__square_mid_integral(f, i, i + delta) for i in np.arange(a, b, delta)])
+    r_sum = sum([__square_right_integral(f, i, i + delta) for i in np.arange(a, b, delta)])
+    return l_sum, m_sum, r_sum
+
+
+def square_integral(f, a, b, eps):
+    n = 16
+    l, m, r = rec_square_integral(f, a, b, n)
+    while abs(max(l, m, r) - min(l, m, r)) - eps > 0:
+        n *= 2
+        l, m, r = rec_square_integral(f, a, b, n)
+    return min(l, m, r), sum((l, m, r)) - max(l, m, r) - min(l, m, r), max(l, m, r), n
+
+
+def trap_integral(f, a, b, eps):
+    n = 16
+    l, m, r = rec_square_integral(f, a, b, n)
+    t = (l + r) / 2
+    t_ = 0
+    while abs(t - t_) > eps:
+        t_ = t
+        n *= 2
+        l, m, r = rec_square_integral(f, a, b, n)
+        t = (l + r) / 2
+    return l, t, r, n
+
+
+def simp_integral(f, a, b, h):
+    return sum([__simp_integral(f, i, i + h) for i in np.arange(a, b, h)])
--- script.py
+++ script.py
@@ -1,10 +1,9 @@
 from JNF_and_basis import *
 from numpy import array as M
 
-# A = M([[2, 0, 0, 0, 0, 0], [0, 2, 0, 0, 0, 0], [4, 0, 1, 0, 0, 0], [0, 1, -3, 9, 0, 0], [0, -3, 1, 0, 9, 0],
-#        [0, 0, 1, 0, -2, 9]])
-#
-# S, J = JNF(A)
-# print(S.round(2))
-# print(J.round(2))
+from Integrals import *
 
+f = lambda x: 1 + x + x**2 + x**3 + x**4
+print(*square_integral(f, -1, 1, 0.001))
+print(*trap_integral(f, -1, 1, 0.001))
+print(simp_integral(f, -1, 1, 1))
